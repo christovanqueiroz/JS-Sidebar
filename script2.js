196fed0e439d7b1462e44ee83dac9de1e0d15188
christovanqueiroz <106829027+christovanqueiroz@users.noreply.github.com>
2022-12-04 15:56:17 -0300

    deleted:    script2.js

--- script2.js
+++
@@ -1,9 +0,0 @@
-const itemName = document.querySelectorAll('.title')
-
-itemName.forEach(item => {
-    item.addEventListener('click', () => {
-        (item.querySelector('.menu')).style.display = 'none';
-    })
-})
-
-console.log(itemName)
