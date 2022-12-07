ccfb834d5c74b1aff47e982fa6ed179ef0777b45
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2022-12-07 13:06:37 -0300

    feat: add selected function

--- script.js
+++ script.js
@@ -1,73 +1,36 @@
-const openSideBar = document.querySelector('.menu');
+const closeSideBar = document.querySelector('.menu');
 const itemName = document.querySelectorAll('.title');
 const logo = document.querySelectorAll('.logo');
 const icon = document.querySelectorAll('.icon');
 const drawer = document.getElementById('drawer');
-let isMenuClosed = true;
+let menuClosed = true;
+const navItem = document.querySelectorAll('aside section nav ul li')
+let navItemSelected = true;
 
 const handleToggleDrawer = () => {
-    if(isMenuClosed) {
-        drawer.classList.add("isMenuClosed");
+    if(menuClosed) {
+        drawer.classList.add("menuClosed");
         itemName.forEach(item => item.classList.add('hide'));
         logo.forEach(item => item.classList.add('hide'));
         icon.forEach(item => item.style.paddingLeft = '0.4em');
-        isMenuClosed = false;
+        menuClosed = false;
     } else {
-        drawer.classList.remove("isMenuClosed");
+        drawer.classList.remove("menuClosed");
         itemName.forEach(item => item.classList.remove('hide'));
         logo.forEach(logos => logos.classList.remove('hide'));
         icon.forEach(item => item.style.paddingLeft = '0.8em');
-        isMenuClosed = true;
+        menuClosed = true;
     }
 }
 
-openSideBar.addEventListener('click', handleToggleDrawer)
-/*    if(isMenuOpen === true) {
-    itemName.forEach(item => {
-        openSideBar.addEventListener('click', () => {
-            item.classList.add("hide");
-        })
-    })
-    
-    logo.forEach(i => {
-        openSideBar.addEventListener('click', () =>{
-            i.classList.add("hide");
-        })
-    })
-    
-    icon.forEach(i => {
-        openSideBar.addEventListener('click', () => {
-            i.style.paddingLeft = '0.4em';
-        })
-    })
-    
-    openSideBar.addEventListener('click', function(){
-        document.querySelector('aside').classList.add("isMenuOpen");
-        isMenuOpen = true;
-    })    
-} else if(isMenuOpen === false) {
-    itemName.forEach(item => {
-        openSideBar.addEventListener('click', () => {
-            item.classList.remove("hide");
-        })
-    })
-    
-    logo.forEach(i => {
-        openSideBar.addEventListener('click', () =>{
-            i.classList.remove("hide");
-        })
-    })
-    
-    icon.forEach(i => {
-        openSideBar.addEventListener('click', () => {
-            i.style.paddingLeft = '0.8rem';
-        })
-    })
-    
-    openSideBar.addEventListener('click', function(){
-        document.querySelector('aside').classList.remove("isMenuOpen");
-        isMenuOpen = false;
-    })
+closeSideBar.addEventListener('click', handleToggleDrawer);
+
+const selectNavItem = () => {
+    if(navItemSelected) {
+        navItem.forEach(item => item.classList.toggle('selected'));
+        navItemSelected = true;
+    }
 }
 
-console.log(isMenuOpen) */
+navItem.forEach(item => item.addEventListener('click', selectNavItem)
+)
